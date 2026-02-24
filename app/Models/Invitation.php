9df09bfa532179c53ee503ab3f0c627d3a8c77c4
bahaztariq <bahaztariq@gmail.com>
2026-02-24 13:42:38 +0000

    Refactor Invitation model to include fillable properties and relationships

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -9,4 +9,27 @@ class Invitation extends Model
 {
     /** @use HasFactory<\Database\Factories\InvitationFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'colocation_id',
+        'email',
+        'token',
+        'status',
+        'sent_at',
+        'responded_at',
+        'expired_at',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function isExpired()
+    {
+        return $this->expired_at && $this->expired_at->isPast();
+    }
+    public function status(){
+        return $this->status;
+    }
 }
