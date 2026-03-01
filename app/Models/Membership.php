868f8520c4bdcec3882ec3c93f929d74ad4ea98a
bahaztariq <bahaztariq@gmail.com>
2026-03-01 15:51:34 +0000

    feat: add Membership pivot model and memberships migration

---
+++ app/Models/Membership.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class Membership extends Pivot
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'colocation_id',
+        'role',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+}
